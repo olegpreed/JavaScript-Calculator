ef12a6956ad8adab29f963a6de71881ea677e7cc
Plummer Reed <preed@at-d4.msk.21-school.ru>
2022-07-19 03:16:54 +0300

    new

--- src/chapter_2/ex2/script.js
+++ src/chapter_2/ex2/script.js
@@ -6,363 +6,357 @@ let finalResult = 0;
 let i = 0;
 let dot = 0;
 let stepen = 1;
-const operation = [0, 0, 0, 0]
+const operation = [0, 0, 0, 0];
 
-const buttonC = document.querySelector('.gridslot:nth-child(2)')
-const button1 = document.querySelector('.gridslot:nth-child(3)')
-const button2 = document.querySelector('.gridslot:nth-child(4)')
-const button3 = document.querySelector('.gridslot:nth-child(5)')
-const buttonPlus = document.querySelector('.gridslot:nth-child(6)')
-const button4 = document.querySelector('.gridslot:nth-child(7)')
-const button5 = document.querySelector('.gridslot:nth-child(8)')
-const button6 = document.querySelector('.gridslot:nth-child(9)')
-const buttonMinus = document.querySelector('.gridslot:nth-child(10)')
-const button7 = document.querySelector('.gridslot:nth-child(11)')
-const button8 = document.querySelector('.gridslot:nth-child(12)')
-const button9 = document.querySelector('.gridslot:nth-child(13)')
-const buttonMultiply = document.querySelector('.gridslot:nth-child(14)')
-const buttonDivide = document.querySelector('.gridslot:nth-child(15)')
-const button0 = document.querySelector('.gridslot:nth-child(16)')
-const buttonDot = document.querySelector('.gridslot:nth-child(17)')
-const buttonEqual = document.querySelector('.gridslot:nth-child(18)')
+const buttonC = document.querySelector(".gridslot:nth-child(2)");
+const button1 = document.querySelector(".gridslot:nth-child(3)");
+const button2 = document.querySelector(".gridslot:nth-child(4)");
+const button3 = document.querySelector(".gridslot:nth-child(5)");
+const buttonPlus = document.querySelector(".gridslot:nth-child(6)");
+const button4 = document.querySelector(".gridslot:nth-child(7)");
+const button5 = document.querySelector(".gridslot:nth-child(8)");
+const button6 = document.querySelector(".gridslot:nth-child(9)");
+const buttonMinus = document.querySelector(".gridslot:nth-child(10)");
+const button7 = document.querySelector(".gridslot:nth-child(11)");
+const button8 = document.querySelector(".gridslot:nth-child(12)");
+const button9 = document.querySelector(".gridslot:nth-child(13)");
+const buttonMultiply = document.querySelector(".gridslot:nth-child(14)");
+const buttonDivide = document.querySelector(".gridslot:nth-child(15)");
+const button0 = document.querySelector(".gridslot:nth-child(16)");
+const buttonDot = document.querySelector(".gridslot:nth-child(17)");
+const buttonEqual = document.querySelector(".gridslot:nth-child(18)");
 
-const result = document.querySelector('.gridslot div span')
+const result = document.querySelector(".gridslot div span");
 
 function equal(event) {
-    if (operation[0] === 1) {
-        if (i === 1) {
-            xy[0] = xy[0] + xy[1]
-            document.querySelector('.gridslot div span').innerText = xy[0]
-            xy[1] = 0
-            i = 0
-        }
-        else {
-            xy[1] = xy[0] + xy[1]
-            document.querySelector('.gridslot div span').innerText = xy[1]
-            xy[0] = 0
-            i = 1
-        }
-        operation[0] = 0
+  if (operation[0] === 1) {
+    if (i === 1) {
+      xy[0] = xy[0] + xy[1];
+      document.querySelector(".gridslot div span").innerText = xy[0];
+      xy[1] = 0;
+      i = 0;
+    } else {
+      xy[1] = xy[0] + xy[1];
+      document.querySelector(".gridslot div span").innerText = xy[1];
+      xy[0] = 0;
+      i = 1;
     }
-    if (operation[1] === 1) {
-        if (i === 1) {
-            xy[0] = xy[0] - xy[1]
-            document.querySelector('.gridslot div span').innerText = xy[0]
-            xy[1] = 0
-            i = 0
-        }
-        else {
-            xy[1] = xy[1] - xy[0]
-            document.querySelector('.gridslot div span').innerText = xy[1]
-            xy[0] = 0
-            i = 1
-        }
-        operation[1] = 0
+    operation[0] = 0;
+  }
+  if (operation[1] === 1) {
+    if (i === 1) {
+      xy[0] = xy[0] - xy[1];
+      document.querySelector(".gridslot div span").innerText = xy[0];
+      xy[1] = 0;
+      i = 0;
+    } else {
+      xy[1] = xy[1] - xy[0];
+      document.querySelector(".gridslot div span").innerText = xy[1];
+      xy[0] = 0;
+      i = 1;
     }
-    if (operation[2] === 1) {
-        if (i === 1) {
-            xy[0] = xy[0] * xy[1]
-            document.querySelector('.gridslot div span').innerText = xy[0]
-            xy[1] = 0
-            i = 0
-        }
-        else {
-            xy[1] = xy[1] * xy[0]
-            document.querySelector('.gridslot div span').innerText = xy[1]
-            xy[0] = 0
-            i = 1
-        }
-        operation[2] = 0
+    operation[1] = 0;
+  }
+  if (operation[2] === 1) {
+    if (i === 1) {
+      xy[0] = xy[0] * xy[1];
+      document.querySelector(".gridslot div span").innerText = xy[0];
+      xy[1] = 0;
+      i = 0;
+    } else {
+      xy[1] = xy[1] * xy[0];
+      document.querySelector(".gridslot div span").innerText = xy[1];
+      xy[0] = 0;
+      i = 1;
     }
-    if (operation[3] === 1) {
-        if (i === 1) {
-            xy[0] = xy[0] / xy[1]
-            document.querySelector('.gridslot div span').innerText = xy[0]
-            xy[1] = 0
-            i = 0
-        }
-        else {
-            xy[1] = xy[1] / xy[0]
-            document.querySelector('.gridslot div span').innerText = xy[1]
-            xy[0] = 0
-            i = 1
-        }
-        operation[3] = 0
+    operation[2] = 0;
+  }
+  if (operation[3] === 1) {
+    if (i === 1) {
+      xy[0] = xy[0] / xy[1];
+      document.querySelector(".gridslot div span").innerText = xy[0];
+      xy[1] = 0;
+      i = 0;
+    } else {
+      xy[1] = xy[1] / xy[0];
+      document.querySelector(".gridslot div span").innerText = xy[1];
+      xy[0] = 0;
+      i = 1;
     }
-    stepen = 1
+    operation[3] = 0;
+  }
+  stepen = 1;
 }
 
 function plusuem(event) {
-    console.log(xy[0], xy[1], i)
-    if (i == 0) i = 1
-    else i = 0
-    operation[0] = 1
-    stepen = 1
-    dot = 0
+  console.log(xy[0], xy[1], i);
+  if (i == 0) i = 1;
+  else i = 0;
+  operation[0] = 1;
+  stepen = 1;
+  dot = 0;
 }
 
 function reset(event) {
-    xy = [0, 0]
-    stepen = 1
-    document.querySelector('.gridslot div span').innerText = xy[i]
+  xy = [0, 0];
+  stepen = 1;
+  document.querySelector(".gridslot div span").innerText = xy[i];
 }
 
 function multiplaem(event) {
-    if (i == 0) i = 1
-    else i = 0
-    operation[2] = 1
-    stepen = 1
-    dot = 0
+  if (i == 0) i = 1;
+  else i = 0;
+  operation[2] = 1;
+  stepen = 1;
+  dot = 0;
 }
 
 function delim(event) {
-    if (i == 0) i = 1
-    else i = 0
-    operation[3] = 1
-    stepen = 1
-    dot = 0
+  if (i == 0) i = 1;
+  else i = 0;
+  operation[3] = 1;
+  stepen = 1;
+  dot = 0;
 }
 
 function vichitaem(event) {
-    if (i == 0) i = 1
-    else i = 0
-    operation[1] = 1
-    stepen = 1
-    dot = 0
+  if (i == 0) i = 1;
+  else i = 0;
+  operation[1] = 1;
+  stepen = 1;
+  dot = 0;
 }
 
 function tochka(event) {
-    console.log(xy[0], xy[1], i)
-    document.querySelector('.gridslot div span').append(".")
-    dot = 1
+  console.log(xy[0], xy[1], i);
+  document.querySelector(".gridslot div span").append(".");
+  dot = 1;
 }
 
-function putone(event) {
-    console.log(xy[0], xy[1], i)
-    if (!dot)  xy[i] = xy[i] * 10 + 1;
-    else {
-        xy[i] = xy[i] +  0.1 / stepen;
-        stepen = stepen * 10;
-    }
-    document.querySelector('.gridslot div span').innerText = xy[i]
-    document.querySelector('.gridslot div span').style.color = 'black'
+function putOne(event) {
+  console.log(xy[0], xy[1], i);
+  if (!dot) xy[i] = xy[i] * 10 + 1;
+  else {
+    xy[i] = xy[i] + 0.1 / stepen;
+    stepen = stepen * 10;
+  }
+  document.querySelector(".gridslot div span").innerText = xy[i];
+  document.querySelector(".gridslot div span").style.color = "black";
 }
 
-buttonEqual.addEventListener('click', equal)
+function putTwo(event) {
+  if (!dot) xy[i] = xy[i] * 10 + 2;
+  else {
+    xy[i] = xy[i] + 0.2 / stepen;
+    stepen = stepen * 10;
+  }
+  document.querySelector(".gridslot div span").innerText = xy[i];
+  document.querySelector(".gridslot div span").style.color = "black";
+}
 
-buttonPlus.addEventListener('click', plusuem)
+function putThree(event) {
+  if (!dot) xy[i] = xy[i] * 10 + 3;
+  else {
+    console.log(xy[i], stepen, i);
+    xy[i] = xy[i] + 0.3 / stepen;
+    stepen = stepen * 10;
+  }
+  document.querySelector(".gridslot div span").innerText = xy[i];
+  document.querySelector(".gridslot div span").style.color = "black";
+}
 
-buttonMultiply.addEventListener('click', multiplaem)
+function putFour(event) {
+  if (!dot) xy[i] = xy[i] * 10 + 4;
+  else {
+    console.log(xy[i], stepen, i);
+    xy[i] = xy[i] + 0.4 / stepen;
+    stepen = stepen * 10;
+  }
+  document.querySelector(".gridslot div span").innerText = xy[i];
+  document.querySelector(".gridslot div span").style.color = "black";
+}
 
-buttonDivide.addEventListener('click', delim)
+function putFive(event) {
+  if (!dot) xy[i] = xy[i] * 10 + 5;
+  else {
+    console.log(xy[i], stepen, i);
+    xy[i] = xy[i] + 0.5 / stepen;
+    stepen = stepen * 10;
+  }
+  document.querySelector(".gridslot div span").innerText = xy[i];
+  document.querySelector(".gridslot div span").style.color = "black";
+}
 
-buttonMinus.addEventListener('click', vichitaem)
+function putSix(event) {
+  if (!dot) xy[i] = xy[i] * 10 + 6;
+  else {
+    console.log(xy[i], stepen, i);
+    xy[i] = xy[i] + 0.6 / stepen;
+    stepen = stepen * 10;
+  }
+  document.querySelector(".gridslot div span").innerText = xy[i];
+  document.querySelector(".gridslot div span").style.color = "black";
+}
 
-buttonC.addEventListener('click', reset)
+function putSeven(event) {
+  if (!dot) xy[i] = xy[i] * 10 + 7;
+  else {
+    console.log(xy[i], stepen, i);
+    xy[i] = xy[i] + 0.7 / stepen;
+    stepen = stepen * 10;
+  }
+  document.querySelector(".gridslot div span").innerText = xy[i];
+  document.querySelector(".gridslot div span").style.color = "black";
+}
 
-buttonDot.addEventListener('click', tochka)
+function putEight(event) {
+  if (!dot) xy[i] = xy[i] * 10 + 8;
+  else {
+    console.log(xy[i], stepen, i);
+    xy[i] = xy[i] + 0.8 / stepen;
+    stepen = stepen * 10;
+  }
+  document.querySelector(".gridslot div span").innerText = xy[i];
+  document.querySelector(".gridslot div span").style.color = "black";
+}
 
-button1.addEventListener('click', putone)
+function putNine(event) {
+  if (!dot) xy[i] = xy[i] * 10 + 9;
+  else {
+    console.log(xy[i], stepen, i);
+    xy[i] = xy[i] + 0.9 / stepen;
+    stepen = stepen * 10;
+  }
+  document.querySelector(".gridslot div span").innerText = xy[i];
+  document.querySelector(".gridslot div span").style.color = "black";
+}
 
+function putZero(event) {
+  xy[i] = xy[i] * 10;
+  document.querySelector(".gridslot div span").innerText = xy[i];
+  document.querySelector(".gridslot div span").style.color = "black";
+}
 
-addEventListener('keydown', (e) => {
-    console.log(e.key)
-    if (e.key === "=" && !e.shiftKey)
-        equal()
-    else if (e.key === '+')
-        plusuem()
-    else if (e.key === 'Backspace' || e.key === 'Clear' )
-        reset()
-    else if (e.key === '*')
-        multiplaem()
-    else if (e.key === '-')
-        vichitaem()
-    else if (e.key === '/')
-        delim()
-    else if (e.key === '.')
-        tochka()
-        else if (e.key === '.')
-        tochka()
-        else if (e.key === '.')
-        tochka()
-        else if (e.key === '.')
-        tochka()
-        else if (e.key === '.')
-        tochka()
-        else if (e.key === '.')
-        tochka()
-        else if (e.key === '.')
-        tochka()
-        else if (e.key === '.')
-        tochka()
-        else if (e.key === '.')
-        tochka()
-        else if (e.key === '.')
-        tochka()
-})
+button2.addEventListener("click", putTwo);
 
+button3.addEventListener("click", putThree);
 
+button4.addEventListener("click", putFour);
 
-button2.addEventListener('click', function (event) {
-    if (!dot)  xy[i] = xy[i] * 10 + 2;
-    else {
-        xy[i] = xy[i] +  0.2 / stepen;
-        stepen = stepen * 10;
-    }
-    document.querySelector('.gridslot div span').innerText = xy[i]
-    document.querySelector('.gridslot div span').style.color = 'black'
-})
+button5.addEventListener("click", putFive);
 
-button3.addEventListener('click', function (event) {
-    if (!dot)  xy[i] = xy[i] * 10 + 3;
-    else {
-        console.log(xy[i], stepen, i)
-        xy[i] = xy[i] +  0.3 / stepen;
-        stepen = stepen * 10;
-    }
-    document.querySelector('.gridslot div span').innerText = xy[i]
-    document.querySelector('.gridslot div span').style.color = 'black'
-})
+button6.addEventListener("click", putSix);
 
-button4.addEventListener('click', function (event) {
-    if (!dot)   xy[i] = xy[i] * 10 + 4;
-    else {
-        console.log(xy[i], stepen, i)
-        xy[i] = xy[i] +  0.4 / stepen;
-        stepen = stepen * 10;
-    }
-    document.querySelector('.gridslot div span').innerText = xy[i]
-    document.querySelector('.gridslot div span').style.color = 'black'
-})
+button7.addEventListener("click", putSeven);
 
-button5.addEventListener('click', function (event) {
-    if (!dot)   xy[i] = xy[i] * 10 + 5;
-    else {
-        console.log(xy[i], stepen, i)
-        xy[i] = xy[i] +  0.5 / stepen;
-        stepen = stepen * 10;
-    }
-    document.querySelector('.gridslot div span').innerText = xy[i]
-    document.querySelector('.gridslot div span').style.color = 'black'
-})
+button8.addEventListener("click", putEight);
 
-button6.addEventListener('click', function (event) {
-    if (!dot)   xy[i] = xy[i] * 10 + 6;
-    else {
-        console.log(xy[i], stepen, i)
-        xy[i] = xy[i] +  0.6 / stepen;
-        stepen = stepen * 10;
-    }
-    document.querySelector('.gridslot div span').innerText = xy[i]
-    document.querySelector('.gridslot div span').style.color = 'black'
-})
+button9.addEventListener("click", putNine);
 
-button7.addEventListener('click', function (event) {
-    if (!dot)   xy[i] = xy[i] * 10 + 7;
-    else {
-        console.log(xy[i], stepen, i)
-        xy[i] = xy[i] +  0.7 / stepen;
-        stepen = stepen * 10;
-    }
-    document.querySelector('.gridslot div span').innerText = xy[i]
-    document.querySelector('.gridslot div span').style.color = 'black'
-})
+button0.addEventListener("click", putZero);
 
-button8.addEventListener('click', function (event) {
-    if (!dot)   xy[i] = xy[i] * 10 + 8;
-    else {
-        console.log(xy[i], stepen, i)
-        xy[i] = xy[i] +  0.8 / stepen;
-        stepen = stepen * 10;
-    }
-    document.querySelector('.gridslot div span').innerText = xy[i]
-    document.querySelector('.gridslot div span').style.color = 'black'
-})
+buttonEqual.addEventListener("click", equal);
 
-button9.addEventListener('click', function (event) {
-    if (!dot)   xy[i] = xy[i] * 10 + 9;
-    else {
-        console.log(xy[i], stepen, i)
-        xy[i] = xy[i] +  0.9 / stepen;
-        stepen = stepen * 10;
-    }
-    document.querySelector('.gridslot div span').innerText = xy[i]
-    document.querySelector('.gridslot div span').style.color = 'black'
-})
+buttonPlus.addEventListener("click", plusuem);
 
-button0.addEventListener('click', function (event) {
-    xy[i] = xy[i] * 10;
-    document.querySelector('.gridslot div span').innerText = xy[i]
-    document.querySelector('.gridslot div span').style.color = 'black'
-})
+buttonMultiply.addEventListener("click", multiplaem);
 
+buttonDivide.addEventListener("click", delim);
+
+buttonMinus.addEventListener("click", vichitaem);
+
+buttonC.addEventListener("click", reset);
+
+buttonDot.addEventListener("click", tochka);
+
+button1.addEventListener("click", putOne);
+
+addEventListener("keydown", (e) => {
+  console.log(e.key);
+  if (e.key === "=" && !e.shiftKey) equal();
+  else if (e.key === "+") plusuem();
+  else if (e.key === "Backspace" || e.key === "Clear") reset();
+  else if (e.key === "*") multiplaem();
+  else if (e.key === "-") vichitaem();
+  else if (e.key === "/") delim();
+  else if (e.key === ".") tochka();
+  else if (e.key === "1") putOne();
+  else if (e.key === "2") putTwo();
+  else if (e.key === "3") putThree();
+  else if (e.key === "4") putFour();
+  else if (e.key === "5") putFive();
+  else if (e.key === "6") putSix();
+  else if (e.key === "7") putSeven();
+  else if (e.key === "8") putEight();
+  else if (e.key === "9") putNine();
+});
 
 function one(callback) {
-    if (callback && typeof callback === 'function') {
-        return (callback(1));
-    } else return 1;
+  if (callback && typeof callback === "function") {
+    return callback(1);
+  } else return 1;
 }
 function two(callback) {
-    if (callback && typeof callback === 'function') {
-        return (callback(2));
-    } else return 2;
+  if (callback && typeof callback === "function") {
+    return callback(2);
+  } else return 2;
 }
 function three(callback) {
-    if (callback && typeof callback === 'function') {
-        return (callback(3));
-    } else return 3;
+  if (callback && typeof callback === "function") {
+    return callback(3);
+  } else return 3;
 }
 function four(callback) {
-    if (callback && typeof callback === 'function') {
-        return (callback(4));
-    } else return 4;
+  if (callback && typeof callback === "function") {
+    return callback(4);
+  } else return 4;
 }
 function five(callback) {
-    if (callback && typeof callback === 'function') {
-        return (callback(5));
-    } else return 5;
+  if (callback && typeof callback === "function") {
+    return callback(5);
+  } else return 5;
 }
 function six(callback) {
-    if (callback && typeof callback === 'function') {
-        return (callback(6));
-    } else return 6;
+  if (callback && typeof callback === "function") {
+    return callback(6);
+  } else return 6;
 }
 function seven(callback) {
-    if (callback && typeof callback === 'function') {
-        return (callback(7));
-    } else return 7;
+  if (callback && typeof callback === "function") {
+    return callback(7);
+  } else return 7;
 }
 function eight(callback) {
-    if (callback && typeof callback === 'function') {
-        return (callback(8));
-    } else return 8;
+  if (callback && typeof callback === "function") {
+    return callback(8);
+  } else return 8;
 }
 function nine(callback) {
-    if (callback && typeof callback === 'function') {
-        return (callback(9));
-    } else return 9;
+  if (callback && typeof callback === "function") {
+    return callback(9);
+  } else return 9;
 }
 
 function plus(a) {
-    return (b) => {
-        return (a + b);
-    }
+  return (b) => {
+    return a + b;
+  };
 }
 
 function minus(a) {
-    return (b) => {
-        return (a - b);
-    }
+  return (b) => {
+    return a - b;
+  };
 }
 
 function divide(a) {
-    return (b) => {
-        return (a / b);
-    }
+  return (b) => {
+    return a / b;
+  };
 }
 
 function mult(a) {
-    return (b) => {
-        return (a * b);
-    }
+  return (b) => {
+    return a * b;
+  };
 }
